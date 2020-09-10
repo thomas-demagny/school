4387d6933222100ee45f1e02ad4be4dc01d97d81
thomas <demagny.t@hotmail.fr>
2020-09-10 11:16:39 +0200

    add some dependencies

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import {} from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text>Salut à tous!</Text>
+        </View>
+        <View>
+         <Text>Comment ça va ?</Text>
+        </View>
+
+        <StatusBar style="auto"/>
+      </SafeAreaView>
   );
 }
 
@@ -19,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
